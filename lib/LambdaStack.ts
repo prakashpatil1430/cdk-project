75f522a5995b95dbabddac965a48a8f41d9bb344
prakash-patil <prakashpatilmeti@gmail.com>
2025-06-10 14:29:18 +0530

    new commit

--- lib/LambdaStack.ts
+++ lib/LambdaStack.ts
@@ -1,16 +1,30 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import { Stack, StackProps} from 'aws-cdk-lib';
+import { Stack, StackProps } from 'aws-cdk-lib';
+import { Runtime } from "aws-cdk-lib/aws-lambda";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import { join } from "path";
 
 
 interface LambdaStackProps extends StackProps {
     stageName?: string
 }
 
-export class LambdaStack extends Stack{
-  constructor(scope: Construct, id: string, props?: LambdaStackProps) {
-    super(scope, id, props);
+export class LambdaStack extends Stack {
+    constructor(scope: Construct, id: string, props?: LambdaStackProps) {
+        super(scope, id, props);
 
-    
-  }
+        new NodejsFunction(this, 'hello-lambda', {
+            runtime: Runtime.NODEJS_18_X,
+            handler: 'handler',
+            entry: (join(__dirname, '..', 'services', 'hello.ts')),
+            environment: {
+                STAGE: props?.stageName!
+            }
+
+
+        })
+
+
+    }
 }
