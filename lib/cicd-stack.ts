b07046bbd9413597d79c6bcf4f887f56e938382c
prakash-patil <prakashpatilmeti@gmail.com>
2025-06-10 13:18:08 +0530

    updated cicd pipeline

--- lib/cicd-stack.ts
+++ lib/cicd-stack.ts
@@ -1,10 +1,24 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines'
 
 
 export class CicdStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    new CodePipeline(this, "cidepipelineid", {
+        pipelineName: "cidepipelineid",
+        synth: new ShellStep('Synth', {
+            input: CodePipelineSource.gitHub('prakashpatil1430/cdk-project', 'cicd-practice'),
+            commands:[
+                'npm ci',
+                'npx cdk synth'
+            ]}
+        )
+    });
+
+    
 
    
   }
